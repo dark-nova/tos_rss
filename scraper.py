5acd52611e50b24df5077ae9b1b1afe902342187
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-04-12 18:45:25 +0000

    Begin date processing

--- scraper.py
+++ scraper.py
@@ -4,9 +4,16 @@ import requests
 with open('example.mhtml', 'r') as example:
     soup = BeautifulSoup(example, 'html.parser')
 
-for news in soup.find_all('div', '3D"news_box"'):
-    #print(news)
-    pass
+news = soup.find_all('div', '3D"news_box"')
+
+for _news in news:
+    inner = _news.find('div', '3D"box_inner"')
+    news_date = inner.find('div', '3D"date')
+    try:
+        news_date = news_date.string.lstrip()
+        print(news_date)
+    except AttributeError as e:
+        print('Skipped, exception: ', e)
 
 # print(len(soup.find_all('div', '3D"news_box"')))
 # output: 9
