6212a544aeca9bbebd1ae28b4a6081e55d231d5b
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-04-12 18:05:13 +0000

    Simplify the code a bit for now

--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-with open('example.mhtml', 'r') as example_file:
-    example = example_file.read()
-
-soup = BeautifulSoup(example, 'html.parser')
+with open('example.mhtml', 'r') as example:
+    soup = BeautifulSoup(example, 'html.parser')
 
 for news in soup.find_all('div', '3D"news_box"'):
     #print(news)
     pass
+
+# print(len(soup.find_all('div', '3D"news_box"')))
+# output: 9
