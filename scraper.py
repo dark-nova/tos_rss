c95c5368ac37f90a6c117449df4237dccba6dcf5
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-04-12 19:13:06 +0000

    Change months list to dict

--- scraper.py
+++ scraper.py
@@ -7,20 +7,19 @@ from bs4 import BeautifulSoup
 
 non_numbers = re.compile('[^0-9]')
 
-months = []
-months.append('0') # sentinel value
-months.append(re.compile('^jan', re.IGNORECASE))
-months.append(re.compile('^feb', re.IGNORECASE))
-months.append(re.compile('^mar', re.IGNORECASE))
-months.append(re.compile('^apr', re.IGNORECASE))
-months.append(re.compile('^may', re.IGNORECASE))
-months.append(re.compile('^jun', re.IGNORECASE))
-months.append(re.compile('^jul', re.IGNORECASE))
-months.append(re.compile('^aug', re.IGNORECASE))
-months.append(re.compile('^sep', re.IGNORECASE))
-months.append(re.compile('^oct', re.IGNORECASE))
-months.append(re.compile('^nov', re.IGNORECASE))
-months.append(re.compile('^dec', re.IGNORECASE))
+months = {}
+months[1] = re.compile('^jan', re.IGNORECASE)
+months[2] = re.compile('^feb', re.IGNORECASE)
+months[3] = re.compile('^mar', re.IGNORECASE)
+months[4] = re.compile('^apr', re.IGNORECASE)
+months[5] = re.compile('^may', re.IGNORECASE)
+months[6] = re.compile('^jun', re.IGNORECASE)
+months[7] = re.compile('^jul', re.IGNORECASE)
+months[8] = re.compile('^aug', re.IGNORECASE)
+months[9] = re.compile('^sep', re.IGNORECASE)
+months[10] = re.compile('^oct', re.IGNORECASE)
+months[11] = re.compile('^nov', re.IGNORECASE)
+months[12] = re.compile('^dec', re.IGNORECASE)
 
 # adapted from https://docs.python.org/3/howto/logging-cookbook.html
 logger = logging.getLogger('tos_scraper')
@@ -48,7 +47,8 @@ for _news in news:
     inner = _news.find('div', '3D"box_inner"')
     news_date = inner.find('div', '3D"date')
     try:
-        news_date = news_date.string.lstrip()
+        news_date = news_date.string.lstrip().split()
+        #for month in months[1:]:
         print(news_date)
     except AttributeError as e:
         logger.warning('Caught exception {} from {}, inner {}'
